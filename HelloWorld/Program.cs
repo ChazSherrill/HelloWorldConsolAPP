7087605549fcd2c3802784b75aabb10f8c9bf416
Chaz Sherrill <chazsherrill@icloud.com>
2020-08-20 10:39:24 -0500

    PUshy pushy

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,11 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("I will conquer World!-Definitely not batman - Me-Michael Scott");
-=======
             Console.WriteLine("I will conquer World!-Wayne Gretzky -Or batman Me-Michael Scott ");
->>>>>>> testBranch
         }
     }
 }
