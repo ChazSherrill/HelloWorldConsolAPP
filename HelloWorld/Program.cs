3714ac0e3089b97207b7ad5d7dffe00e16d67748
Chaz Sherrill <chazsherrill@icloud.com>
2020-08-20 10:30:05 -0500

    modify who said what

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I will conquer World!-Wayne Gretzky - Me-Michael Scott");
+            Console.WriteLine("I will conquer World!-Wayne Gretzky -Or batman Me-Michael Scott ");
         }
     }
 }
