19d279611d7de84a51b58b9ffc5e820c75a746e4
Chaz Sherrill <chazsherrill@icloud.com>
2020-08-20 10:26:25 -0500

    Michael Scott

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I will conquer World!-Wayne Gretzky - Me");
+            Console.WriteLine("I will conquer World!-Wayne Gretzky - Me-Michael Scott");
         }
     }
 }
