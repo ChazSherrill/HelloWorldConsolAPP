adf693b2a2112a5890978357c6e1beeb33d34f2c
Chaz Sherrill <chazsherrill@icloud.com>
2020-08-20 10:22:07 -0500

    Hello World Conq Wayne

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I will conquer World!-Wayne Gretzky - Me");
+        }
+    }
+}
